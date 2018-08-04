7861d8b7fe03ce978c0a19a0c7a6f9178860d6a4
Wang Chao <watcher.hangzhou@foxmail.com>
2018-08-04 16:52:26 +0800

    fix

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("", name="workflow_admin_dashboard")
+     * @Route("", name="default")
      */
     public function index(Request $request)
     {
