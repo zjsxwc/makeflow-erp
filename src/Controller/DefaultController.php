53eaa147daf27582b4ceee215c4bf57eab16f4bc
Wang Chao <watcher.hangzhou@foxmail.com>
2018-08-04 15:55:37 +0800

    first commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,40 +15,6 @@ class DefaultController extends Controller
      */
     public function index(Request $request)
     {
-        $em = $this->get("doctrine.orm.entity_manager");
-        $cdnUrl = $this->getParameter("cdn_url");
-//        dump($em, $cdnUrl);die;
-
-        $mm = $this->get("App\Makeflow\MakeflowManager");
-
-        $paoMianMakeflow = $mm->getMakeflow("PaoMian");
-
-        $mayWorkspaceList = $paoMianMakeflow->getWorkspaces();
-        if (isset($mayWorkspaceList[0])) {
-            $workspace = $mayWorkspaceList[0];
-        }else {
-            $workspace = $paoMianMakeflow->createWorkspace(1, strval(time()));
-        }
-
-//        dump($workspace->getBuildDate()->getTimezone(), $workspace->getBuildDate()->getTimestamp(), $workspace->getBuildDate()->format('Y-m-d H:i:s'), date_default_timezone_get());die;
-
-
-//        $context = $this->get("App\Makeflow\WorkspaceContextFactory")->getContext($workspace);
-//
-//        $cps = $context->getCurrentProcessingPlaces();
-//
-//        $onePlace = $cps[0];
-//
-//        $onePlace->bindUsersToPlace([3,4,2,1]);
-//
-//        dump($cps, $workspace,$onePlace->getUsers());die;
-
-        $paoMianPlaces = $paoMianMakeflow->getPlaces();
-
-        $buyPaoMianPlace = $paoMianPlaces["BuyPaoMian"];
-        return $buyPaoMianPlace->processAction($request, $workspace);
-
-//        dump($paoMianMakeflow, $paoMianMakeflow->getMakeflowConfig(), $paoMianMakeflow->getPlaces());die;
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
